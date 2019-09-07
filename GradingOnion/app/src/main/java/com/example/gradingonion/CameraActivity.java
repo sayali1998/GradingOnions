233b79894257bd18cdc1ceddbe1abcc7c93cffaa
sayali1998 <sayali.patil16@siesgst.ac.in>
2019-09-07 21:49:45 +0530

    Upload Image to Firebase

---
+++ GradingOnion/app/src/main/java/com/example/gradingonion/CameraActivity.java
@@ -0,0 +1,154 @@
+package com.example.gradingonion;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class CameraActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
+    Button uploadButton;
+    Uri photoUri;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    String currentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_camera);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        try
+        {
+            this.getSupportActionBar().hide();
+
+        }
+        catch (NullPointerException e){}
+
+        dispatchTakePictureIntent();
+
+        imageView=findViewById(R.id.imageView);
+        uploadButton=findViewById(R.id.upload);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                uploadImage();
+                Intent intent=new Intent(CameraActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            try {
+                createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        photoUri = Uri.parse(currentPhotoPath);
+        return image;
+    }
+
+    private void uploadImage() {
+
+        Log.d("Upload: ",photoUri.toString());
+
+        if(photoUri != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(photoUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(CameraActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(CameraActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+}
--- ServoArm/ServoArm.ino
+++ ServoArm/ServoArm.ino
@@ -5,31 +5,80 @@
 int servo1=3;
 int servo2=5;
 int servo3=6;
+int servo4=9;
+int servo5=10;
+int servo6=11;
 
 Servo finger1;
 Servo finger2;
 Servo finger3;
+Servo elbow;
+Servo shoulder;
+Servo bottom;
 
 void setup() {
   // put your setup code here, to run once:
   finger1.attach(servo1);
   finger2.attach(servo2);
   finger3.attach(servo3);
+  elbow.attach(servo4);
+  shoulder.attach(servo5);
+  bottom.attach(servo6);
   
 }
 
+void pickUp()
+{
+  elbow.write(90);
+  delay(2000);  
+  
+  int i;
+  for(i=90;i>=0;i--)
+  {
+    finger1.write(i);
+    finger2.write(i);
+    finger3.write(i);
+    }
+  delay(1000);
+  
+  }
+
+void drop()
+{
+  int i;
+  elbow.write(70);
+  delay(2000);
+  bottom.write(90);
+  delay(2000);  
+  for(i=0;i<=90;i++)
+  {
+    finger1.write(i);
+    finger2.write(i);
+    finger3.write(i);
+    }
+  delay(1000);
+  
+  }
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  finger1.write(0);
-  delay(2000);
-  finger2.write(0);
-  delay(2000);
-  finger3.write(0);
-  delay(2000);
   finger1.write(90);
   delay(2000);
   finger2.write(90);
   delay(2000);
   finger3.write(90);
   delay(2000);
+  shoulder.write(0);
+  delay(2000);  
+  elbow.write(0);
+  delay(2000);
+  pickUp();
+  delay(5000);
+  //Turn to drop
+  drop();
+  delay(2000);
+  bottom.write(0);
+  delay(2000);  
+    
 }
