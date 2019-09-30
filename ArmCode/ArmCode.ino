953dc650ff18b71d5d8d2422323a16bdbe2091e8
sayali1998 <sayali.patil16@siesgst.ac.in>
2019-09-30 12:37:25 +0530

    Arduino Code updated

--- ArmCode/ArmCode.ino
+++ ArmCode/ArmCode.ino
@@ -4,8 +4,8 @@
 
 int x;
 int y;
-int h;
-int distCovered=0;   //Distance covered when it moves by 1 degree
+float h=45;
+int distCovered=0.5;   //Distance covered when it moves by 1 degree
 float angle;
 int degree;
 float roundOffAngle;
@@ -15,7 +15,7 @@ int servo_base=3;
 int servo_length_rack=5;
 int servo_depth_rack=6;
 int servo_hold=9;
-int servo_syringe=10;
+int servo_hold2=10;
 
 String coords;
 
@@ -23,11 +23,11 @@ Servo servoBase;
 Servo servoLengthRack;
 Servo servoDepthRack;
 Servo servoHold;
-Servo servoSyringe;
+Servo servoHold2;
 
 int distanceSensor=A0;
 
-SoftwareSerial mySerial(0, 1);
+SoftwareSerial mySerial(10, 11);
 
 void setup() {
   Serial.begin(9600);
@@ -35,10 +35,11 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   servoBase.attach(servo_base);
-  servoLengthRack.attach(servo_length_rack);
+  servoLengthRack.writeMicroseconds(500);
+  servoLengthRack.attach(servo_length_rack,500,3500);
   servoDepthRack.attach(servo_depth_rack);
   servoHold.attach(servo_hold);
-  servoSyringe.attach(servo_syringe);
+  servoHold2.attach(servo_hold2);
   pinMode(distanceSensor,INPUT);
   Serial.println("Communication Started");
   mySerial.begin(9600);
@@ -48,27 +49,34 @@ void setup() {
 void initServo()
 {
   servoBase.write(0);
-  servoLengthRack.write(0);
   servoDepthRack.write(0);
   servoHold.write(0);
-  servoSyringe.write(0);
+  servoHold2.write(100);
+  coords="";
   delay(1000);
   }
 
 void loop() {
-
+  
   initServo();
   //Serial Communication x,y values
-  if (mySerial.available()) {
-    coords=mySerial.readString();
-    Serial.println(coords);
-  }
+//  if (mySerial.available()) {
+//    coords=mySerial.readString();
+//    Serial.println(coords);
+//    x=coords[0];
+//    y=coords[1];
+// }
   
-//  h=sqrt((x*x)+(y*y));
-//  angle=atan(h)*57.2958;
-//
-//  servoBase.write(angle);
-//
+  //h=sqrt((x*x)+(y*y));
+  angle=atan(h)*57.2958;
+  int j;
+  for(j=0;j<45;j++)
+  {
+    servoBase.write(j);
+    delay(50);
+    }
+  delay(2000);
+
 //  roundOffAngle=h/distCovered;
 //  if(roundOffAngle>=(int(roundOffAngle)+0.5))
 //  {
@@ -78,32 +86,40 @@ void loop() {
 //    {
 //      distanceMovedAngle=int(roundOffAngle);
 //    }
-//  servoLengthRack.write(distanceMovedAngle);
-//  delay(2000);
+  
+//  servoLengthRack.write(180);
+//  delay(3000);
 //  degree=0;
-//  while(analogRead(distanceSensor)>1)
-//  {
-//    servoDepthRack.write(degree++);
-//    delay(100);
-//    }
-//  servoDepthRack.write(degree+3);
-//  delay(100);
-//
-//  servoSyringe.write(180);
-//  delay(1000);
-//
-//  servoDepthRack.write(degree-10);
-//  delay(100);
-//
-//  servoHold.write(100);
-//  delay(1000);
-//
-//  servoBase.write(120);
-//  delay(1000);
-//
-//  servoSyringe.write(0);
-//  delay(1000);
-//
-//  servoHold.write(0);
-//  delay(500);
+//  for(;degree<90;degree++)
+//    {
+//      servoDepthRack.write(degree);
+//      delay(1000);
+//      }
+  int i;
+  for(i=0;i<120;i++)
+  {
+    servoHold.write(i);
+    servoHold2.write(120-i);
+    delay(50);
+    }
+  int k;
+    for(k=50;k<100;k++)
+    {
+  servoBase.write(k); 
+  delay(50);   
+      }
+  
+  delay(1000);
+
+  servoHold2.write(90);
+
+  delay(1000);
+
+  servoHold.write(0);
+  delay(500);
+  for(k=100;k>0;k--)
+    {
+  servoBase.write(k); 
+  delay(50);   
+      }
 }
